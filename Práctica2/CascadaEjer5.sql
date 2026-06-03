a770b8a6a8c2508599a6d43dd47c8de11baf6dea
carolvillan2-ship-it <carolvillan2@gmail.com>
2026-06-03 12:06:00 -0400

    Create CascadaEjer5.sql

---
+++ Práctica2/CascadaEjer5.sql
@@ -0,0 +1,10 @@
+CREATE TABLE productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    categoria_id INT,
+
+    FOREIGN KEY (categoria_id)
+    REFERENCES categorias(id)
+    ON DELETE CASCADE
+);
